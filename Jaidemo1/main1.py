84263b3cc700f22f847a1363a1b5e07cc009ed7e
jban1 <74945709+jban1@users.noreply.github.com>
2022-01-12 20:38:10 +0530

    Add files via upload

--- Jaidemo1/main1.py
+++ Jaidemo1/main1.py
@@ -1,19 +1,3 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python38_app]
-# [START gae_python3_app]
 import logging
 
 from flask import Flask, request
@@ -32,6 +16,16 @@ logging.basicConfig(level=logging.INFO)
 # called `app` in `main.py`.
 app = Flask(__name__)
 
+@app.route('/')
+def Hello():
+    """Return a friendly HTTP greeting."""
+    return '''
+        Hello! Enter a string to reverse it.
+        <form method="get" action="reverse_string">
+            <p><input type=text name=string value="abcd">
+            <p><input type=submit>
+        </form>
+    '''
 
 # There is a bug in the code.
 class StringProcessor():
@@ -75,16 +69,7 @@ def ReverseString():
     '''.format(current, expected)
 
 
-@app.route('/')
-def Hello():
-    """Return a friendly HTTP greeting."""
-    return '''
-        Hello! Enter a string to reverse it.
-        <form method="get" action="reverse_string">
-            <p><input type=text name=string value="abcd">
-            <p><input type=submit>
-        </form>
-    '''
+
 
 
 if __name__ == '__main__':
@@ -92,5 +77,3 @@ if __name__ == '__main__':
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
